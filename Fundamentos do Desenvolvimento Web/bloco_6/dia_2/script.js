2dfd9359c0fa29e0ba58699675704098647f1de8
Daniela Muller <danielamuller20@gmail.com>
2021-01-07 09:57:06 -0300

    requisitos do formulário

--- Fundamentos do Desenvolvimento Web/bloco_6/dia_2/script.js
+++ Fundamentos do Desenvolvimento Web/bloco_6/dia_2/script.js
@@ -4,7 +4,7 @@ function selectState(states) {
   const selectStates = document.querySelector('#select-estado');
   for (let index = 0; index < states.length; index += 1) {
     const options = document.createElement('option');
-    options.setAttribute('required', 'required');
+    options.setAttribute('data-validate-field', 'state');
     options.setAttribute('name', 'estado');
     options.innerHTML = states[index];
     selectStates.appendChild(options)
@@ -25,6 +25,24 @@ function checkEmail() {
   console.log(emails);
 }
 
+function checkCPF() {
+  let append = document.querySelector('#cpf');
+  const div = document.createElement('div');
+  let input = document.querySelector('#input-cpf')
+  input.addEventListener('input', function () {
+    let cpf = document.querySelector('#input-cpf').value;
+    cpf = parseInt(cpf);
+    if (isNaN(cpf)) {
+      div.innerHTML = 'Enter a valid CPF. Only numbers';
+      div.style.color = '#B81111';
+    } else {
+      div.innerHTML = '';
+    }
+    append.appendChild(div);
+  });
+}
+checkCPF();
+
   var picker = new Pikaday({
     field: document.getElementById('datepicker'),
     format: 'D MMM YYYY',
@@ -32,3 +50,99 @@ function checkEmail() {
       console.log(this.getMoment().format('Do MMMM YYYY'));
     }
   });
+
+  new JustValidate('.js-form', {
+    rules: {
+      cpf: {
+        required: true,
+        minLength: 11,
+        maxLength: 11
+      },
+      address: {
+        required: true,
+        maxLength: 300
+      },
+      city: {
+        required: true,
+        maxLength: 28
+      },
+      state: {
+        required: true
+      },
+      myField: {
+        required: true
+      },
+      text: {
+        required: true,
+        maxLength: 1000
+      },
+      job: {
+        required: true,
+        maxLength: 40
+      },
+      description: {
+        required: true,
+        maxLength:500
+      },
+      date: {
+        required: true
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        strength: {
+          default: true,
+        }
+    },
+    messages: {
+      name: {
+        minLength: 'My custom message about only minLength rule'
+      },
+      cpf: {
+        minLength: 'My custom message about only minLength rule',
+        maxLength: 'My custom message about only maxLength rule'
+      },
+      address: {
+        maxLength: 'My custom message about only minLength rule'
+        },
+      city: {
+        maxLength: 'My custom message about only minLength rule'
+        },
+      text: {
+        maxLength: 'My custom message about only minLength rule'
+        },
+      job: {
+        maxLength: 'My custom message about only minLength rule'
+        },
+      description: {
+        maxLength: 'My custom message about only minLength rule'
+        },
+      email: 'My custom message about error (one error message for all rules)'
+    },
+
+    submitHandler: function (form, values, ajax) {
+
+      ajax({
+        url: 'https://just-validate-api.herokuapp.com/submit',
+        method: 'POST',
+        data: values,
+        async: true,
+        callback: function(response)  {
+          console.log(response)
+        }
+      });
+    },
+  },
+});
+
+function getInputs() {
+  let allInputs = document.querySelectorAll('input');
+  let inputs = [];
+  for (let index = 0; index < allInputs.length; index += 1) {
+    inputs.push(allInputs[index].value);
+  }
+  console.log(inputs)
+}
+getInputs();
