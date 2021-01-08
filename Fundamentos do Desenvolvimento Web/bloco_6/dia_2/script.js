91770bb7c860cc3f1c2c5ec632d4523068b1a288
Daniela Muller <danielamuller20@gmail.com>
2021-01-08 18:57:59 -0300

    Formulario pronto

--- Fundamentos do Desenvolvimento Web/bloco_6/dia_2/script.js
+++ Fundamentos do Desenvolvimento Web/bloco_6/dia_2/script.js
@@ -1,3 +1,5 @@
+const require = ("promise-polyfill");
+
 const statesOfBrazil = ['Selecione seu estado', 'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 
 function selectState(states) {
@@ -25,29 +27,29 @@ function checkEmail() {
   console.log(emails);
 }
 
-function checkCPF() {
-  let append = document.querySelector('#cpf');
-  const div = document.createElement('div');
-  let input = document.querySelector('#input-cpf')
-  input.addEventListener('input', function () {
-    let cpf = document.querySelector('#input-cpf').value;
-    cpf = parseInt(cpf);
-    if (isNaN(cpf)) {
-      div.innerHTML = 'Enter a valid CPF. Only numbers';
-      div.style.color = '#B81111';
-    } else {
-      div.innerHTML = '';
-    }
-    append.appendChild(div);
-  });
-}
-checkCPF();
+// function checkCPF() {
+//   let append = document.querySelector('#cpf');
+//   const div = document.createElement('div');
+//   let input = document.querySelector('#input-cpf')
+//   input.addEventListener('input', function () {
+//     let cpf = document.querySelector('#input-cpf').value;
+//     cpf = parseInt(cpf);
+//     if (isNaN(cpf)) {
+//       div.innerHTML = 'Enter a valid CPF. Only numbers';
+//       div.style.color = '#B81111';
+//     } else {
+//       div.innerHTML = '';
+//     }
+//     append.appendChild(div);
+//   });
+// }
+// checkCPF();
 
   var picker = new Pikaday({
     field: document.getElementById('datepicker'),
     format: 'D MMM YYYY',
     onSelect: function() {
-      console.log(this.getMoment().format('Do MMMM YYYY'));
+      this.getMoment().format('Do MMMM YYYY');
     }
   });
 
@@ -102,7 +104,7 @@ checkCPF();
       },
       cpf: {
         minLength: 'My custom message about only minLength rule',
-        maxLength: 'My custom message about only maxLength rule'
+        maxLength: 'My custom message about only maxLength rule',
       },
       address: {
         maxLength: 'My custom message about only minLength rule'
@@ -137,12 +139,69 @@ checkCPF();
   },
 });
 
-function getInputs() {
-  let allInputs = document.querySelectorAll('input');
-  let inputs = [];
-  for (let index = 0; index < allInputs.length; index += 1) {
-    inputs.push(allInputs[index].value);
-  }
-  console.log(inputs)
+// function getInputs() {
+//   let allInputs = document.querySelectorAll('.text');
+//   let inputs = [];
+//   for (let index = 0; index < allInputs.length; index += 1) {
+//     if (allInputs[index].checked) {
+//       inputs.push(allInputs[index].checked)
+//     }else if (allInputs[index].getAttribute('type', 'text'))
+//     inputs.push(allInputs[index].value);
+//   }
+//   return inputs;
+// }
+
+let nameInput = document.querySelector('#input-nome');
+let emailInput = document.querySelector('#input-email');
+let cpfInput = document.querySelector('#input-cpf');
+let addressInput = document.querySelector('#input-endereco');
+let cityInput = document.querySelector('#input-cidade');
+let optionInput = document.querySelector('#select-estado')
+let textareaInput = document.querySelector('.textarea');
+let jobInput = document.querySelector('#input-cargo');
+let descriptionInput = document.querySelector('#input-descricao');
+let dateinput = document.querySelector('#datepicker');
+
+function getInputValues(inpuType) {
+  return inpuType.value
 }
-getInputs();
+
+function getRadioInputValue() {
+  let checked = document.querySelector('input[name="complemento"]:checked');
+  if (checked !== null) {
+    return checked.value;
+  }
+}
+
+function appendData() {
+  const div = document.querySelector('#result-data');
+  while (div.hasChildNodes()) {
+    div.removeChild(div.lastChild);
+  }
+  const h1 = document.createElement('h1');
+  h1.innerHTML = 'Currículo';
+  h1.className = 'title is-1 label content has-text-centered';
+  div.appendChild(h1);  
+  let data = new Map();
+  data.set('Nome: ', getInputValues(nameInput));
+  data.set('E-mail: ', getInputValues(emailInput));
+  data.set('CPF: ', getInputValues(cpfInput));
+  data.set('Endereço: ', getInputValues(addressInput));
+  data.set('Cidade: ', getInputValues(cityInput));
+  data.set('Estado: ', getInputValues(optionInput));
+  data.set('Complemento: ', getRadioInputValue());
+  data.set('Resumo do Currículo: ', getInputValues(textareaInput));
+  data.set('Cargo: ', getInputValues(jobInput));
+  data.set('Descrição: ', getInputValues(descriptionInput));
+  data.set('Data de Início: ', getInputValues(dateinput)); 
+  document.getElementById("myForm").action = "/action_page.php";
+  
+  for (var [key, value] of data) {
+    let paragraph = document.createElement('p');
+    paragraph.innerHTML = (`${key} ${value}`);
+     div.appendChild(paragraph);
+  }
+}
+
+const button = document.querySelector('#button-send');
+button.addEventListener('click', appendData);
