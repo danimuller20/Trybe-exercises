508c8251d92e928b35a2f8d59f02aeacb46aac46
Daniela Muller <danielamuller20@gmail.com>
2021-02-10 15:58:13 -0300

    todo-list with react

--- Desenvolvimento Front-end/dia_1/todo-list-app/src/App.js
+++ Desenvolvimento Front-end/dia_1/todo-list-app/src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
+const tasks = ['Estudar', 'Fazer compras', 'Cozinhar', 'Lavar a louça'];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul> { tasks.map((task) => <li>{ task }</li>) } </ul>
   );
 }
 
