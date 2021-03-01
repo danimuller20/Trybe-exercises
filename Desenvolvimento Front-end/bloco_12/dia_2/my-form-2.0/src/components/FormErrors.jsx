b7d71776c7b624205038c14f4c0ee1357ecb2b55
Daniela Muller <danielamuller20@gmail.com>
2021-03-01 12:24:16 -0300

    exercicios bloco 12 dia2

---
+++ Desenvolvimento Front-end/bloco_12/dia_2/my-form-2.0/src/components/FormErrors.jsx
@@ -0,0 +1,20 @@
+import { Component, Fragment } from 'react';
+
+class FormErrors extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() { 
+    const { errors } = this.props;
+    return (
+      <Fragment>
+        {Object.keys(errors).map((fieldName, i) => {
+          if (errors[fieldName].length === 0) return '';
+          return <p key={i}>{fieldName} {errors[fieldName]}</p>
+        })}
+      </Fragment>
+    );
+  }
+}
+ 
+export default FormErrors;
